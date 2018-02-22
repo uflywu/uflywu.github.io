dad01e310328b241fd314d826c3cd4c96a34eb46
wflywu <uflywu@163.com>
2018-02-22 14:55:49 +0800

    optimize image loading speed.

--- compile/index.js
+++ compile/index.js
@@ -1,6 +1,9 @@
 const fs = require("fs")
 const path = require("path")
 const sizeOf = require('image-size');
+const needle = require('needle')
+const md5 = require('nodejs-md5');
+const jsonfile = require('jsonfile')
   
 const root = path.join(__dirname, '../photo/')
 const target = path.join(__dirname, '../data.json')
@@ -21,56 +24,131 @@ function readDirSync(dir) {
   })  
 }
 
-
-
-function readGalleries(root) {
+async function readGalleries(root, lastData) {
+  const lastDataMap = {}
   const result = []
   const f = fs.readdirSync(root);
-  f.forEach(function(name, index) {
+
+  lastData.forEach(d => {
+    lastDataMap[d.date] = d.data
+    d.data.imagesMap = {}
+    d.data.images.forEach(img => d.data.imagesMap[img.name] = img)
+  });
+  
+  for (let i = 0; i < f.length; i++) {
+    const name = f[i];
     const currentPath = root + "/" + name
     const info = fs.statSync(currentPath)
     if (info.isDirectory()) {
+      let data = await readGallery(currentPath, lastDataMap[name])
       result.push({
         date: name,
-        data: readGallery(currentPath)
+        data
       })
     }
-  })
+  }
   return result
 }
 
-function readGallery(galleryPath) {
+async function readGallery(galleryPath, lastData) {
   const f = fs.readdirSync(galleryPath);
   const data = {
     images: []
   }
-  f.forEach(function(name, index) {
+  for (let i = 0; i < f.length; i++) {
+    const name = f[i];
     const currentPath = galleryPath + "/" + name
     const info = fs.statSync(currentPath)
-    let dimensions
+
     if (!info.isDirectory() && !name.startsWith('.')) {
       if (/.*(jpg|png|jpeg|gif)$/i.test(name)) {
-        dimensions = sizeOf(currentPath);
-        data.images.push({
-          name,
-          width: dimensions.width,
-          height: dimensions.height
-        })
+        const imageMd5 = await getFileMd5(currentPath)
+        
+        if (lastData && imageMd5 === lastData.imagesMap[name].md5) {
+          data.images.push(lastData.imagesMap[name])
+        } else {
+          const dimensions = sizeOf(currentPath);
+          console.info('uploading file', name)
+          try {
+            const url = await uploadImage(currentPath)
+            data.images.push({
+              name,
+              md5: imageMd5,
+              url,
+              width: dimensions.width,
+              height: dimensions.height
+            })
+          } catch (error) {
+           console.error('add image failed.', name) 
+          }
+        }
       } else if (/.*(txt)$/i.test(name)) {
         const desc = fs.readFileSync(currentPath, 'utf-8').split('\n')
         data.title = desc.shift();
         data.detail = desc.join('\n')
       }
     }
-  })
+  }
   return data
 }
 
-const result = readGalleries(root)
-// console.info(JSON.stringify(result))
-fs.writeFile(target, JSON.stringify(result), function(err) {
-  if(err) {
-      return console.log(err);
+function uploadImage(filePath) {
+  return new Promise(function (resolve, reject) {
+    var data = {
+      callback: 'callback',
+      upload: { file: filePath, content_type: 'image/jpeg' }
+    }
+    needle.post('https://file-dd.jd.com/file/uploadImg.action', data, { multipart: true }, function(err, resp, body) {
+      if (err || !body) {
+        return reject(err)
+      }
+      const reg = body.match(/path":"([^"]*)"/)
+      if (reg && reg.length) {
+        resolve(reg[1])
+      } else {
+        reject(body)
+      }
+    });
+  });
+}
+
+function getFileMd5(filePath) {
+  return new Promise(function (resolve, reject) {
+    md5.file.quiet(filePath, function(err, md5) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(md5)
+      }
+    });
+  });
+}
+
+// async function start(params) {
+//   // let mm = await getFileMd5("/Users/even/Dev/uflywu/photo/20180220/IMG_20180220_104616_meitu.jpg")
+//   // console.info(mm)
+
+//   let url = await uploadImage('/Users/even/Dev/uflywu/photo/20180220/IMG_20180220_104616_meitu.jpg')
+//   console.info(url)
+// }
+// start()
+
+function launch() {
+  jsonfile.readFile(target, function(err, data) {
+    start(data);
+  })
+
+  async function start(data) {
+    const lastData = data || []
+    const result = await readGalleries(root, lastData)
+    // console.info(JSON.stringify(result))
+    fs.writeFile(target, JSON.stringify(result), function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("build successed!");
+    });
   }
-  console.log("build successed!");
-});
+}
+
+launch()
